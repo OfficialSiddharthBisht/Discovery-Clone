a0dfa0ec0e014e141e9c796d4ba3d3ab89dbad87
Siddharth <officialsiddharthbisht@gmail.com>
2022-05-26 19:57:53 +0530

    kids page started

---
+++ Scripts/kids.js
@@ -0,0 +1,39 @@
+let container = document.getElementById("card-container");
+let url = "http://localhost:3000/kids";
+
+let headings = ["Everyone's Watching" , "Little Singham VS Kaal" , "Best of Little Singham" ,"Top Toons", "Animated Blockbusters", "Fun Science And Engineering" ,"Discovery Our Animal Friends"];
+
+function fetchData(){
+    fetch(url)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((response)=>{
+        displayCard(response);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+}
+
+function displayCard(data){
+    let curr = 0;
+   for(let i = 0; i < headings.length;i++){
+        container.append(headings[i])
+        let cardRow = document.createElement("div");
+       for(let j = curr; j < curr + 4; j++){
+        cardRow.setAttribute("class","card-row");
+        let card = document.createElement("div");
+        card.setAttribute("class","card");
+        let title = document.createElement("p");
+        title.innerText = data[j].title;
+        let image= document.createElement("img");
+        image.src = data[j].img;
+        card.append(title,image);
+        cardRow.append(card);
+       }
+       container.append(cardRow) 
+       curr = curr + 4;
+   }
+}
+fetchData();
